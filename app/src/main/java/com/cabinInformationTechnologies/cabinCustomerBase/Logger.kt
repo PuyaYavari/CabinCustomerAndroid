ceb44c32156a2cc7e6d9fca84e93d005ff39d375
root <pouyayavari73@gmail.com>
2019-10-28 16:39:10 +0300

    Crashlytics added to Logger

--- app/src/main/java/com/cabinInformationTechnologies/cabinCustomerBase/Logger.kt
+++ app/src/main/java/com/cabinInformationTechnologies/cabinCustomerBase/Logger.kt
@@ -2,6 +2,8 @@ package com.cabinInformationTechnologies.cabinCustomerBase
 
 import android.content.Context
 import android.util.Log
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.analytics.FirebaseAnalytics
 
 object Logger: BaseContracts.Logger {
     override fun debug(
@@ -28,10 +30,13 @@ object Logger: BaseContracts.Logger {
         exception: Throwable
     ) {
         try {
-            if (location != null)
-                Log.e("Error","LOCATION: $location \nMESSAGE: $message", exception)
-            else
+            if (location != null) {
+                Log.e("Error", "LOCATION: $location \nMESSAGE: $message", exception)
+                Crashlytics.log(Log.ERROR, "Error", "LOCATION: $location \nMESSAGE: $message \nEXCEPTION: ${exception.toString()}" )
+            } else {
                 Log.e("Error", message, exception)
+                Crashlytics.log(Log.ERROR, "Error", "MESSAGE: $message \nEXCEPTION: ${exception.toString()}" )
+            }
             //TODO: SEND HANDLE THE REST
         } catch (exception: Exception) {
             failure(context, this::class.java.name, null, exception)
@@ -78,10 +83,27 @@ object Logger: BaseContracts.Logger {
         message: String,
         exception: Exception?) {
         try {
-            if (location != null)
-                Log.w("Warn", "LOCATION: $location \nMESSAGE: $message", exception)
-            else
-                Log.w("Warn", message, exception)
+            if (location != null) {
+                if (exception != null) {
+                    Log.w("Warn", "LOCATION: $location \nMESSAGE: $message", exception)
+                    Crashlytics.log(
+                        Log.WARN,
+                        "WARN",
+                        "LOCATION: $location \nMESSAGE: $message \nEXCEPTION: ${exception.toString()}"
+                    )
+                } else {
+                    Log.w("Warn", "LOCATION: $location \nMESSAGE: $message", null)
+                    Crashlytics.log(Log.WARN, "WARN", "LOCATION: $location \nMESSAGE: $message")
+                }
+            } else {
+                if (exception != null) {
+                    Log.w("Warn", message, exception)
+                    Crashlytics.log(Log.WARN, "WARN", "MESSAGE: $message \nEXCEPTION: ${exception.toString()}" )
+                } else {
+                    Log.w("Warn", message, null)
+                    Crashlytics.log(Log.WARN, "WARN", "MESSAGE: $message" )
+                }
+            }
             //TODO: SEND HANDLE THE REST
         } catch (exception: Exception) {
             failure(context, this::class.java.name, null, exception)
@@ -94,13 +116,22 @@ object Logger: BaseContracts.Logger {
         message: String?,
         exception: Exception?
     ) {
-        if (location != null && message != null && exception != null)
+        if (location != null && message != null && exception != null) {
             Log.wtf("Failure", "LOCATION: $location \nMESSAGE: $message", exception)
-        else if (exception != null)
+            Crashlytics.log(Log.ERROR, "WTF", "MESSAGE: $message \nEXCEPTION: ${exception.toString()}" )
+        } else if (exception != null) {
             Log.wtf("Failure", null, exception)
-        else
+            Crashlytics.log(Log.ERROR, "WTF", "EXCEPTION: ${exception.toString()}" )
+        } else {
             Log.wtf("Failure", "Unexpected Problem", null)
+            Crashlytics.log(Log.ERROR, "WTF", "" )
+        }
         //TODO: SEND HANDLE THE REST
+    }
+
+    private fun firebaseLog(context: Context) {
+        var firebaseAnalytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+        //TODO: SEND TO ANALYTICS
     }
 
 
